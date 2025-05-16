c0cb5ca31410f12c12668a364a782f99d5feb275
Kethu <Yerramma.Kethu@adib.com>
2025-05-16 16:09:57 +0400

    Tab Component moved to separate package

---
+++ app/src/main/java/com/example/windowscompactexample/testing/ShowTabs.kt
@@ -0,0 +1,40 @@
+package com.example.windowscompactexample.testing
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.windowscompactexample.components.IconTextTabs
+
+@Composable
+     fun ShowTabs() {
+        val tabs = listOf(
+            Icons.Default.Favorite to "Cards",
+            Icons.Default.AccountBox to "Finance",
+            Icons.Default.AccountCircle to "Takaful"
+        )
+
+        Column {
+            var selectedTab by remember { mutableIntStateOf(0) }
+            var selectedTab1 by remember { mutableIntStateOf(0) }
+
+            IconTextTabs(
+                tabs = tabs,
+                selectedIndex = selectedTab,
+                onTabSelected = { selectedTab = it }
+            )
+
+            IconTextTabs(
+                tabs = tabs,
+                selectedIndex = selectedTab1,
+                onTabSelected = { selectedTab1 = it },
+                isRippleEnable = false
+            )
+        }
+    }
