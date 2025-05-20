d32296e4bb9b4e3f8879c4efd2a7c59fc1eaf231
Kethu <Yerramma.Kethu@adib.com>
2025-05-20 13:44:28 +0400

    Check boxes implemented

--- app/src/main/java/com/example/windowscompactexample/MainActivity.kt
+++ app/src/main/java/com/example/windowscompactexample/MainActivity.kt
@@ -8,106 +8,44 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.windowscompactexample.components.Details
-import com.example.windowscompactexample.components.OtpVerificationState
-import com.example.windowscompactexample.components.UiAuthPin
 import com.example.windowscompactexample.testing.ChipGroupDemo
+import com.example.windowscompactexample.testing.RadioButtonSample
+import com.example.windowscompactexample.testing.RadioGroupSample
+import com.example.windowscompactexample.testing.ShowAuthPin
+import com.example.windowscompactexample.testing.ShowCheckBoxes
 import com.example.windowscompactexample.testing.ShowTabs
-import com.example.windowscompactexample.ui.theme.ColorSemanticErrorTwo
-import com.example.windowscompactexample.ui.theme.ColorTextSubdued
-import com.example.windowscompactexample.ui.theme.Dimens.SizeFontTwo
-import com.example.windowscompactexample.ui.theme.StyleCaptionRegular
+import com.example.windowscompactexample.testing.YesNoToggleSwitch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                        Spacer(modifier = Modifier.height(32.dp))
-                        ChipGroupDemo()
-                        ShowTabs()
-                        Spacer(modifier = Modifier.height(32.dp))
-                        Details()
-                        Spacer(modifier = Modifier.height(32.dp))
-                        ShowAuthPin()
-                    }
-
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                    Spacer(modifier = Modifier.height(64.dp))
+                    ShowCheckBoxes()
+                   // Spacer(modifier = Modifier.height(32.dp))
+                   // RadioButtonSample()
+                  //  Spacer(modifier = Modifier.height(32.dp))
+                  //  RadioGroupSample()
+                    Spacer(modifier = Modifier.height(32.dp))
+                    ChipGroupDemo()
+                    ShowTabs()
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Details()
+                    Spacer(modifier = Modifier.height(32.dp))
+                    ShowAuthPin()
+                    Spacer(modifier = Modifier.height(32.dp))
+                    YesNoToggleSwitch()
                 }
             }
         }
-}
-
-@Composable
-private fun ShowAuthPin() {
-    var otp: String by remember { mutableStateOf("") }
-    var verificationState by remember { mutableStateOf(OtpVerificationState.DEFAULT) }
-    val descBoldTextStyle = SpanStyle(
-        color = ColorTextSubdued,
-        fontSize = SizeFontTwo.sp,
-        fontWeight = FontWeight.SemiBold
-    )
-
-    val normalText = SpanStyle(
-        color = ColorTextSubdued,
-        fontSize = StyleCaptionRegular.fontSize,
-        fontWeight = StyleCaptionRegular.fontWeight
-    )
-    val defaultAnnString = buildAnnotatedString {
-        withStyle(style = normalText) {
-            append("Enter the One Time Password (OTP) sent on your mobile number ending with ")
-        }
-        withStyle(style = descBoldTextStyle) {
-            append("1234.")
-        }
     }
-
-    val errorAnnString = buildAnnotatedString {
-        withStyle(style = normalText.copy(ColorSemanticErrorTwo)) {
-            append("Something went wrong. Please try again or request a new OTP.")
-        }
-    }
-
-    var description by remember { mutableStateOf(defaultAnnString) }
-
-    Spacer(modifier = Modifier.height(32.dp))
-    UiAuthPin(
-        modifier = Modifier.padding(20.dp),
-        description = description,
-        otpVerificationState = verificationState,
-        actionText = "Resend OTP",
-        onTextChange = {
-            if (verificationState == OtpVerificationState.ERROR) {
-                description = defaultAnnString
-                verificationState = OtpVerificationState.DEFAULT
-            } else {
-                verificationState = OtpVerificationState.TYPING
-            }
-        },
-        onActionTextClicked = {
-            verificationState = OtpVerificationState.ERROR
-            description = errorAnnString
-        },
-        onPinComplete = {
-            otp = it
-        },
-    )
 }
---
+++ app/src/main/java/com/example/windowscompactexample/testing/ShowCheckBoxes.kt
@@ -0,0 +1,45 @@
+package com.example.windowscompactexample.testing
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.windowscompactexample.components.UiCheckBox
+
+@Composable
+fun ShowCheckBoxes() {
+    UiCheckBox(
+        title = "Header here",
+        subTitle = "Sub header here",
+    ) { checked,subTitle ->
+        println("Kethu1 $checked $subTitle")
+    }
+    Spacer(modifier = Modifier.height(16.dp))
+    UiCheckBox(
+        subTitle = "Sub header here",
+    ){ checked,subTitle ->
+        println("Kethu2 $checked $subTitle")
+    }
+
+    Spacer(modifier = Modifier.height(16.dp))
+    val listNames = listOf("Sub header here1", "Sub header here2", "Sub header here3")
+
+
+    var listNames1 = mutableSetOf<String>()
+    listNames.forEach {
+        UiCheckBox(
+            modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp),
+            subTitle = it,
+        ) { checked, subTitle ->
+            println("Kethu3 $checked $subTitle")
+            if (checked) {
+                listNames1.add(subTitle)
+            } else {
+                listNames1 = listNames1.filter {  subTitle == it }.toMutableSet()
+            }
+            println("Kethu3 listNames1 $listNames1")
+        }
+    }
+}
