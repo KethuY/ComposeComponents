4262bafc2ee57b5ce70168d29d5f0e8cd683eb6a
Kethu <Yerramma.Kethu@adib.com>
2025-05-21 15:03:59 +0400

    Chips implemented

--- app/src/main/java/com/example/windowscompactexample/MainActivity.kt
+++ app/src/main/java/com/example/windowscompactexample/MainActivity.kt
@@ -4,28 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.windowscompactexample.components.UiChip
 import com.example.windowscompactexample.components.UiMultiSelectionChip
@@ -34,8 +49,10 @@ import com.example.windowscompactexample.testing.ShowAuthPin
 import com.example.windowscompactexample.testing.ShowTabs
 import com.example.windowscompactexample.ui.theme.ColorContainer
 import com.example.windowscompactexample.ui.theme.ColorInteraction
+import com.example.windowscompactexample.ui.theme.ColorSemanticSuccessTwo
 import com.example.windowscompactexample.ui.theme.ColorTextBase
 import com.example.windowscompactexample.ui.theme.StyleBodyMedium
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,6 +86,7 @@ class MainActivity : ComponentActivity() {
                     Spacer(modifier = Modifier.height(32.dp))
                     ShowAuthPin()
                     Spacer(modifier = Modifier.height(32.dp))
+                    CustomSnackbarScaffold()
                 }
             }
         }
@@ -171,5 +189,66 @@ class MainActivity : ComponentActivity() {
                 println("Kethu $it")
             },
         )
+    }
+}
+
+
+@Composable
+fun CustomSnackbarScaffold() {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState) {
+                CustomSnackBarContent()
+            }
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = {
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar("",  duration = SnackbarDuration.Short)
+                    }
+                }
+            ) {
+                Text("Show Snackbar")
+            }
+        }
+    ) { padding ->
+        Box(modifier = Modifier.padding(padding))
+    }
+}
+
+
+@Composable
+fun CustomSnackBarContent() {
+    Surface(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        color = ColorSemanticSuccessTwo,
+        shape = RoundedCornerShape(12.dp),
+        tonalElevation = 6.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Beneficiary added successfully",
+                color = Color.White,
+                textAlign = TextAlign.Start,
+                modifier = Modifier.weight(1f)
+            )
+
+            Icon(
+                imageVector = Icons.Default.Info,
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
     }
 }
